84ee09ebe31f31178028fd69c1e050b7f9e3f0d0
Julie Faure-Lacroix <julie.faure-lacroix@calculquebec.ca>
2016-05-17 14:38:35 -0400

    Ajout de fichiers qui vont servir a l'ecole d'ete MCF 2016

---
+++ EPB_dataset/epb_select_opt.R
@@ -0,0 +1,36 @@
+##################################################################
+#####    Model Selection using AIC and EPB dataset           ##### 
+#####           V.1 = SOMEWHAT BETTER OPTIONS                #####
+##################################################################
+# we want to investigate the effect of drainage, simpson's index, balsam fir defoliation by spruce budworm within a variable-radius plot (wedge prism), age of the epidemy, 
+# light reaching the middle part of the tree, and distance to the nearest large river on dbh, height, h/d ratio, and volume of white spruce.
+# we want to select the best model using AIC
+# set working directory
+setwd("/path/to/working/directory")
+
+# load packages
+packgs<-c("rJava", "glmulti", "lme4")
+lapply(packgs, library, character.only=TRUE)
+
+# laod datasets
+tree_df<-read.csv("./epb_tree_dataset.csv", header=T)
+
+# INTRODUCING LOOPS AND LISTS
+#list of variables
+yvar.list<-list("dbh", "height", "ratio_hd", "volume") # 3 response variables
+xvar<-"classe_drainage+D_index+defoliation+epid_age+light+dist_from_river"# several explanatory variables which will be combined later
+output.list=list()
+
+lmer.wrap<-function(formula,data,random="",...){
+  lmer(paste(deparse(formula),random),data=data, REML=FALSE,...)
+}
+
+t.st<-Sys.time()
+for(y in yvar.list){
+  out<-glmulti(formula(paste(y, "scale(classe_drainage)+scale(D_index)+scale(defoliation)+scale(epid_age)+scale(light)+scale(dist_from_river)", sep="~")), 
+                data=tree_df, random="+(1|type)", fitfunc=lmer.wrap, intercept=TRUE, confsetsize = 10, level=1)
+  output.list[[y]]<-out@objects[[1]]
+}
+print(t.fin-t.st)
+
+
---
+++ EPB_dataset/epb_select_sup2.R
@@ -0,0 +1,44 @@
+############################
+#### Supercomputer file #2
+############################
+
+#####################
+# load packages
+#####################
+packgs<-c("rJava", "glmulti", "lme4")
+lapply(packgs, library, character.only=TRUE)
+
+####################
+# load data
+###################
+tree_df<-read.csv("./epb_tree_dataset.csv", header=T)
+
+####################
+# create list
+# of formulas
+####################
+yvar.list<-list("dbh", "height", "ratio_hd", "volume") # 3 response variables
+xvar<-"classe_drainage+D_index+defoliation+epid_age+light+dist_from_river"# several explanatory variables which will be combined later
+
+####################
+# model selection
+# analysis
+####################
+
+lmer.wrap<-function(formula,data,random="",...){
+  lmer(paste(deparse(formula),random),data=data, REML=FALSE,...)
+}
+
+par_function <- function(y) {
+  name<-paste(y)
+  out <- tryCatch({
+    glmulti(formula(paste(y, "scale(classe_drainage)+scale(D_index)+scale(defoliation)+scale(epid_age)+scale(light)+scale(dist_from_river)", sep="~")), 
+            data=tree_df, random="+(1|type)", fitfunc=lmer.wrap, intercept=TRUE, confsetsize = 10, level=1)
+  }, error = function(error) {
+    print(paste("ERROR:  ", error))
+    return(NA)
+  })
+  return(out@objects[[1]])
+}
+
+
