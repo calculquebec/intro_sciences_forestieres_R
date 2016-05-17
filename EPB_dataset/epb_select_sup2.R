cc1e33ffe86f8da12b0d64e6033024dc07d9c6da
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2016-05-17 18:19:34 -0400

    Fix bug with function returning S4 object

--- EPB_dataset/epb_select_sup2.R
+++ EPB_dataset/epb_select_sup2.R
@@ -32,13 +32,12 @@ lmer.wrap<-function(formula,data,random="",...){
 par_function <- function(y) {
   name<-paste(y)
   out <- tryCatch({
-    glmulti(formula(paste(y, "scale(classe_drainage)+scale(D_index)+scale(defoliation)+scale(epid_age)+scale(light)+scale(dist_from_river)", sep="~")), 
-            data=tree_df, random="+(1|type)", fitfunc=lmer.wrap, intercept=TRUE, confsetsize = 10, level=1)
+	  glmulti(formula(paste(y, "scale(classe_drainage)+scale(D_index)+scale(defoliation)+scale(epid_age)+scale(light)+scale(dist_from_river)", sep="~")), data=tree_df, random="+(1|type)", fitfunc=lmer.wrap, intercept=TRUE, confsetsize = 10, level=1)
   }, error = function(error) {
-    print(paste("ERROR:  ", error))
-    return(NA)
+	print(paste("ERROR:  ", error))
+  	return(NA)
   })
-  return(out@objects[[1]])
+  return(summary.glmulti(out))
 }
 
 
